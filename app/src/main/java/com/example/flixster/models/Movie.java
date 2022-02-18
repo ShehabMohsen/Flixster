28767666c73e4aeffef2fa896bcb8ab2c715e6e1
ShehabMohsen <smohsen000@citymail.cuny.edu>
2022-02-18 04:14:45 -0500

    Movie trailer preview player added.

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 @Parcel
 public class Movie {
+    int movieId;
     String backdropPath;
     String posterPath;
     String title;
@@ -19,12 +20,14 @@ public class Movie {
     public Movie() {
 
     }
+
     public Movie(JSONObject jsonObject) throws JSONException {
         backdropPath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
         rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -52,4 +55,6 @@ public class Movie {
     }
 
     public double getRating() { return rating; }
+
+    public int getMovieId() { return movieId; }
 }
